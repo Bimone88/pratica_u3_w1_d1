8f6e89b1a2d45518ecc3c7fb632e02043c6b0c55
Simone Manca <simone.manca.98@gmail.com>
2024-01-29 14:21:11 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import "./App.css";
 import ButtonComponent from "./ButtonComponent";
 import ImageComponent from "./ImageComponent";
@@ -5,8 +6,18 @@ import ImageComponent from "./ImageComponent";
 function App() {
   return (
     <div>
-      <ButtonComponent text="CLICCA QUI" />
-      <ImageComponent src="http://placekitten.com/300" alt="gattini" />
+      <div>
+        <ButtonComponent text="CLICCA QUI" />
+        <ImageComponent src="http://placekitten.com/300" alt="gattini" />
+      </div>
+      <div>
+        <ButtonComponent text="CLICCA QUI" />
+        <ImageComponent src="https://placebeard.it/640x360" alt="sparta" />
+      </div>
+      <div>
+        <ButtonComponent text="CLICCA QUI" />
+        <ImageComponent src="https://placebear.com/640/360" alt="gattini" />
+      </div>
     </div>
   );
 }
